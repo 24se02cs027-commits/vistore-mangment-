49e915ec4fdbf1148e0455f4b26ea90bac855c51
patel vinit <24se02cs027@ppsu.ac.in>
2026-05-20 14:26:34 +0530

    Add files via upload

---
+++ vistor mngemnet/script.js
@@ -0,0 +1,12 @@
+function login(){
+
+  let username = document.getElementById("username").value;
+  let password = document.getElementById("password").value;
+
+  if(username=="admin" && password=="1234"){
+    window.location="entry.html";
+  }
+  else{
+    alert("Wrong Login");
+  }
+}
